5cce74478ac5a4c006af607c5b99f163a7b5d326
Bruno Roemers <bruno@roemers.io>
2021-01-04 23:44:56 +0100

    Allow registering commands to `utb`

--- README.md
+++ README.md
@@ -33,8 +33,13 @@ $TOOLBOX_MODULES
 Since `deps.sh` and `load.sh` are ordinary shell scripts and Unix Toolbox sources
 both of them on module load, what they should contain is purely conventional.
 However, it is important to only set up prerequisites in `deps.sh` since this
-provides a clear overview to module users of other scripts that are needed to run the
-module.
+provides a clear overview to module users of the other scripts that are needed to run
+the module.
+
+If `deps.sh` only contains calls to `utb load <module-name>`, circular dependencies
+can be detected before the main code (`load.sh`) is executed. Hence, in case of a
+circular dependency, the main code is not executed and the shell environment is not
+stained.
 
 Conversely, `load.sh` should contain all lines of code (or delegate it to other files)
 that are needed to initialize the module in a user's terminal session.
@@ -44,4 +49,35 @@ that are needed to initialize the module in a user's terminal session.
 Add the following to e.g. `.bashrc`, `.zshrc` or a module's `deps.sh` file:
 ```bash
 utb load module-name
+# or more verbose:
+utb load-module module-name
 ```
+
+Exit code `10` means that the module could not be loaded due to a circular dependency.
+
+### Commands
+`load` in `utb load <module-name>` is already an example of a command.
+
+Modules can add their own commands to the `utb` namespace via the following
+steps:
+
+1. Define a function in `load.sh` that uses the following naming pattern: `__utb_command_my_awesome_command`.
+   I.e. the function starts with `__utb_command_` and all dashes (`-`) are translated to underscores (`_`).
+1. Register the command: `utb load-command my-awesome-command`
+1. Invoke your custom command like this: `utb my-awesome-command`
+
+Exit code `10` means that the command was already registered.
+Exit code `11` means that the associated function could not be found, hence the
+command was not registered.
+
+### Exit Codes
+Codes `0` up to (and including) `9` are reserved for Unix Toolbox.
+
+Currently defined:
+- `0`: success (bash standard)
+- `1`: command not found
+
+Commands are free to assign their own meaning to exit codes starting from number `10`.
+Unix Toolbox will properly pass down any exit codes to the calling shell.
+
+> See also: https://tldp.org/LDP/abs/html/exitcodes.html
--- unix-toolbox.sh
+++ unix-toolbox.sh
@@ -14,16 +14,30 @@ export TOOLBOX_MODULES=${TOOLBOX_MODULES:-"$TOOLBOX_HOME/modules"}
 
 
 # internal state
-export __TOOLBOX_EXIT_CODE=0 # propagate error codes down the stack
 export __TOOLBOX_LOADED_MODULES=()
+export __TOOLBOX_LOADED_COMMANDS=(load load-module load-command)
 export __TOOLBOX_LOADING_STACK=()
+export __TOOLBOX_EXIT_CODE=0 # propagate error codes down the stack
 
 
 # public api
-# usage: utb load <module-name>
+# usage: utb <command> [<arguments> ...]
+# e.g. utb load <module-name>
+# e.g. utb load-command <command-name>
+# return 0 (success), 1 (command not found), > 9 (command-specific exit code)
 utb() {
-  local command=$1
-  [[ $command == "load" ]] && __utb_load_module $2
+  local command="$1"; shift
+  local function="__utb_command_${command//-/_}"
+  if $(type "$function" > /dev/null 2>&1) && __utb_array_contains "$command" "${__TOOLBOX_LOADED_COMMANDS[@]}"
+  then
+    # execute function and capture exit code
+    __TOOLBOX_EXIT_CODE=0; $function $@; __TOOLBOX_EXIT_CODE=$?
+  else
+    # command not found
+    echo "error: command '$command' not found. Is it registered?"
+    __TOOLBOX_EXIT_CODE=1
+  fi
+  return $__TOOLBOX_EXIT_CODE
 }
 
 
@@ -76,8 +90,15 @@ __utb_remove_module_from_stack() {
 }
 
 
-# return 0 (module loaded) or 1 (failed due to circular dependency)
-__utb_load_module() {
+# load a module
+# usage: utb load <module-name>
+#        utb load-module <module-name>
+# => load $TOOLBOX_MODULES/module-name and its dependencies
+# return 0 (success: module loaded) or 10 (failed due to circular dependency)
+__utb_command_load() {
+  __utb_command_load_module $@
+}
+__utb_command_load_module() {
   # inputs
   local module_name="$1"
 
@@ -89,7 +110,7 @@ __utb_load_module() {
   # error if circular dependency
   if __utb_module_is_circular "$module_name"; then
     echo "error: circular dependency: ${__TOOLBOX_LOADING_STACK[@]} $module_name" >&2
-    __TOOLBOX_EXIT_CODE=1
+    __TOOLBOX_EXIT_CODE=10 # failed due to circular dependency
     return $__TOOLBOX_EXIT_CODE
   fi
 
@@ -109,4 +130,24 @@ __utb_load_module() {
   __TOOLBOX_LOADED_MODULES+=("$module_name")
 
   return $__TOOLBOX_EXIT_CODE
+}
+
+# add a new command to the public api
+# usage: utb load-command <command-name>
+# => utb <command-name> will call __utb_command_command_name and pass arguments
+# return 0 (success), 10 (command already registered), 11 (function not found)
+__utb_command_load_command() {
+  local command="$1"
+  if __utb_array_contains "$command" "${__TOOLBOX_LOADED_COMMANDS[@]}"; then
+    echo "error: command '$command' was already registered."
+    return 10
+  fi
+
+  local function="__utb_command_${command//-/_}"
+  if ! $(type "$function" > /dev/null 2>&1); then
+    echo "error: could not find function for command '$command'."
+    return 11
+  fi
+
+  __TOOLBOX_LOADED_COMMANDS+=("$command")
 }
