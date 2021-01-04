01eb815cdfea2d987fd0d621f3a41cdd3c21bdca
Bruno Roemers <bruno@roemers.io>
2021-01-04 21:43:47 +0100

    Write module loader

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Unix Toolbox
-A brave attempt to create a modular system for managing dotfiles, shell scripts, settings and other customizations.
+> A brave attempt to create a modular system for managing dotfiles, shell scripts, settings and other customizations.
+
+## Install
+Clone this repository: `git clone https://github.com/unix-toolbox/unix-toolbox.git $HOME/path/to/install/location`
+
+Add the following to e.g. `.bashrc` or `.zshrc`:
+```bash
+# init unix-toolbox
+TOOLBOX_HOME="$HOME/path/to/install/location"
+source "$TOOLBOX_HOME/unix-toolbox.sh"
+```
+
+Restart your terminal session: e.g. `exec $SHELL`
+
+## Usage
+### Modules
+Unix Toolbox modules are essentially collections of files that provide a well-defined
+and distinct functionality.
+It is adviced to stick to the concept of "Do One Thing And Do It Well."
+This means modules are easy to share and composable.
+
+Modules are located in `$TOOLBOX_MODULES` and have the following structure:
+```bash
+$TOOLBOX_MODULES
+├── module-1
+│   ├── deps.sh # set up all prerequisites for module-1 (sourced) (optional file)
+│   ├── load.sh # load all parts of module-1 into the current shell session (sourced) (optional file)
+│   └── any other files that may be needed
+└── module-2
+    └── ...
+```
+Since `deps.sh` and `load.sh` are ordinary shell scripts and Unix Toolbox sources
+both of them on module load, what they should contain is purely conventional.
+However, it is important to only set up prerequisites in `deps.sh` since this
+provides a clear overview to module users of other scripts that are needed to run the
+module.
+
+Conversely, `load.sh` should contain all lines of code (or delegate it to other files)
+that are needed to initialize the module in a user's terminal session.
+
+
+### Loading a Module
+Add the following to e.g. `.bashrc`, `.zshrc` or a module's `deps.sh` file:
+```bash
+utb load module-name
+```
---
+++ unix-toolbox.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+
+# Source this file to initialize unix-toolbox. Certain commands
+# provided by unix-toolbox will modify the current shell environment.
+# (e.g. add functions, add/modify env vars...)
+
+
+# use user-defined $TOOLBOX_HOME or derive from this file's location
+export TOOLBOX_HOME=${TOOLBOX_HOME:-$(dirname $(realpath ${BASH_SOURCE[0]}))}
+
+# use user-defined $TOOLBOX_MODULES or derive from $TOOLBOX_HOME
+export TOOLBOX_MODULES=${TOOLBOX_MODULES:-"$TOOLBOX_HOME/modules"}
+
+
+# public api
+# usage: utb load <module-name>
+utb() {
+  local command=$1
+  [[ $command == "load" ]] && __utb_load_module $2
+}
+
+
+# internal state
+export __TOOLBOX_EXIT_CODE=0 # propagate error codes down the stack
+export __TOOLBOX_LOADED_MODULES=()
+export __TOOLBOX_DEPENDENCY_STACK=()
+
+
+# return 0 (found) or 1 (not found)
+__utb_array_contains() {
+  # first argument: needle
+  # other arguments: haystack
+  local needle="$1"; shift
+  
+  # loop over remaining arguments
+  local item
+  for item; do
+    [[ "$item" == "$needle" ]] && return 0
+  done
+  return 1
+}
+
+
+__utb_continue() {
+  return $__TOOLBOX_EXIT_CODE
+}
+
+
+# return 0 (loaded) or 1 (not loaded)
+__utb_module_is_loaded() {
+  __utb_array_contains "$1" "${__TOOLBOX_LOADED_MODULES[@]}"
+}
+
+
+# return 0 (circular dependency) or 1 (no circular dependency)
+__utb_module_is_circular() {
+  __utb_array_contains "$1" "${__TOOLBOX_DEPENDENCY_STACK[@]}"
+}
+
+__utb_add_module_to_stack() {
+  local module_name="$1"
+  __TOOLBOX_DEPENDENCY_STACK+=("$module_name")
+}
+
+__utb_remove_module_from_stack() {
+  local module_name="$1"
+  local i tempAry=()
+  for i in "${!__TOOLBOX_DEPENDENCY_STACK[@]}"; do
+    local val="${__TOOLBOX_DEPENDENCY_STACK[i]}"
+    if [[ "$val" != "$module_name" ]]; then
+      tempAry+=("$val")
+    fi
+  done
+  export __TOOLBOX_DEPENDENCY_STACK=("${tempAry[@]}")
+}
+
+
+# return 0 (module loaded) or 1 (failed due to circular dependency)
+__utb_load_module() {
+  # inputs
+  local module_name="$1"
+
+  # skip if module was already loaded
+  if __utb_module_is_loaded "$module_name"; then
+    return $__TOOLBOX_EXIT_CODE
+  fi
+
+  # error if circular dependency
+  if __utb_module_is_circular "$module_name"; then
+    echo "error: circular dependency: ${__TOOLBOX_DEPENDENCY_STACK[@]} $module_name" >&2
+    __TOOLBOX_EXIT_CODE=1
+    return $__TOOLBOX_EXIT_CODE
+  fi
+
+  # set module loading
+  __utb_add_module_to_stack "$module_name"
+
+  # setup module dependencies
+  local module_dependency_path="$TOOLBOX_MODULES/$module_name/deps.sh"
+  __utb_continue && [[ -f $module_dependency_path ]] && source "$module_dependency_path"
+
+  # load module
+  local module_load_path="$TOOLBOX_MODULES/$module_name/load.sh"
+  __utb_continue && [[ -f $module_load_path ]] && source "$module_load_path"
+
+  # set module loaded
+  __utb_remove_module_from_stack "$module_name"
+  __TOOLBOX_LOADED_MODULES+=("$module_name")
+
+  return $__TOOLBOX_EXIT_CODE
+}
